79e55556241cd21e85b2270f8e31dd2309f3105c
Frigzer <borekadam89@gmail.com>
2026-04-12 18:19:11 +0200

    Add ticket_machine_client

---
+++ task1/include/ticket_machine_client.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "ticket_server.hpp"
+#include <string_view>
+
+namespace task1 {
+
+class TicketMachineClient {
+public:
+    explicit TicketMachineClient(TicketServer& server);
+
+    std::vector<TicketAvailability> showAvailableTickets();
+    
+    std::optional<ReservationResult> selectTicket(std::string_view ticket_type);
+    
+    bool cancel(ReservationId reservation_id);
+
+    std::variant<PurchaseSuccess, PurchaseFailure> buy(
+        ReservationId reservation_id,
+        const CustomerData& customer,
+        const CoinInventory& inserted_coins);
+
+private:
+    TicketServer& server_;
+};
+
+} // namespace task1
